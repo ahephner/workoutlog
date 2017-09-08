beecd4f7814862e89ab2aa32c83cdbf2d167e9f3
AJ Hephner <ajhephner178@gmail.com>
2017-09-08 08:33:49 -0400

    everything up to module 23 is now functioning

--- workoutlogserver/routes/log.js
+++ workoutlogserver/routes/log.js
@@ -4,29 +4,30 @@ var Log = sequelize.import('../models/log');
 var User = sequelize.import('../models/user');
 var Definition = sequelize.import('../models/definition');
 
-router.post('/', function(req, res){
-	var description = req.body.log.desc;
-	var result = req.body.log.result;
-	var user = req.user;
-	var definition = req.body.log.def;
+router.post('/', function(req, res) {
+    // req has some body properties that have a username and pwd
+    var description = req.body.log.description; 
+    var result = req.body.log.result; 
+    var user = req.user;
+    var definition = req.body.log.def;
+   
 
-	//Use our sequlize model to create user
-	Log
-		.create({
-			description: description,
-			result: result,
-			owner: user.id,
-			def: definition
-		})
-		  .then(
-		  		function createSuccess(log) {
-		  			res.json(log);
-		  		},
-		  		function createError(err) {
-		  			res.send(500, err.message);
-
-		  		}
-		  	);
+    // Use our sequelize model to create log
+  	Log 
+	    .create({ 
+	    	description: description,
+	    	result: result,
+	    	owner: user.id,
+	    	def: definition
+	    })
+	    .then(
+	    	function createSuccess(log) {
+	    		res.json(log);
+	    	}, 
+		    function createError(err) {
+		        res.send(500, err.message);
+		    }
+	    );
 });
 
 router.get('/', function(req, res) {
@@ -36,16 +37,63 @@ router.get('/', function(req, res) {
 		where: { owner: userid }
 	})
 	.then(
-			function findAllSuccess(data) {
-				//console.log(data)
-				res.json(data);
-			},
-			function findAllError(err) {
-				res.send(500, err.message);
+		function findAllSuccess(data) {
+			// console.log(data);
+			res.json(data);
+		},
+		function findAllError(err) {
+			res.send(500, err.message);
 		}
-      );
+	);
 });
-//delete logs
+
+//This will retrieve one workout specified by the log id
+//This will retrieve one workout specified by the log id
+router.get('/:id', function(req, res) {
+	var data = req.params.id;
+	//console.log(data); here for testing purposes
+	Log
+		.findOne({
+			where: { id: data }
+		}).then(
+			function getSucces(updateData) {
+				res.json(updateData);
+			},
+
+			function getError(err) {
+				res.send(500, err.message);
+			}
+		);
+});
+
+//This will return the data from the log that was updated
+router.put('/', function(req, res) {
+    var description = req.body.log.desc;
+    var result = req.body.log.result; 
+    var data = req.body.log.id;
+    var definition = req.body.log.def;
+    console.log(req);
+    Log
+    	.update(
+    	{
+    		description: description,
+	    	result: result,
+	    	def: definition
+    	},
+
+    	{where: {id: data}}
+    	).then(
+    		function updateSuccess(updatedLog) {
+    			res.json(updatedLog);
+    		},
+
+    		function updateError(err){
+    			res.send(500, err.message);
+    		}
+    	)
+});
+
+
 router.delete('/', function(req, res) {
 	var data = req.body.log.id;
 	Log
@@ -60,6 +108,5 @@ router.delete('/', function(req, res) {
 			}
 		);
 });
-
 
 module.exports = router; 
